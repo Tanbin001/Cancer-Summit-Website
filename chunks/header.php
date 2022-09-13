20ffa3792a1813d6cc8d6c9e13841fafc39fe2be
Tanbin <tanbinsiddiqui11@gmail.com>
2022-09-14 01:45:58 +0600

    Cancer summit

--- chunks/header.php
+++ chunks/header.php
@@ -24,7 +24,7 @@
                 <div>
                 <button
                     class="modal-open transition duration-500 ease-in-out bg-black nav-txt hover:bg-yellow-200 transform hover:-translate-y-1 hover:scale-110 px-6 py-2 mr-2 rounded ring-2 ring-indigo-500 ring-opacity-50 font-bold">
-                    STRATEGY SUMMIT 2022
+                    Cancer SUMMIT 2022
                 </button>
                 <button
                     class="transition duration-500 ease-in-out nav-txt transform hover:-translate-y-1 hover:scale-110 px-6 py-2 ml-3 mr-2 rounded ring-2 ring-indigo-500 ring-opacity-50 font-bold">
